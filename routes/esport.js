df9dea3c731889e1e2f083b5de4ef42fa42ac2f3
Thibaud <abonnel.t@gmail.com>
2023-09-18 14:54:29 +0200

    ok

--- routes/esport.js
+++ routes/esport.js
@@ -3,6 +3,18 @@ var router = express.Router();
 const fetch = require("node-fetch");
 
 
+// URL GET MATCH
+
+// LOL 
+
+const lflMatch ="https://api.pandascore.co/leagues/4292/matches?filter[finished]=false&sort&page=1&per_page=50"
+const lecMatch ="https://api.pandascore.co/leagues/4292/matches?filter[finished]=false&sort&page=1&per_page=50"
+
+// ROCKETLEAGUE
+
+const rlcsMatch ="https://api.pandascore.co/leagues/4834/matches?filter[finished]=false&sort&page=1&per_page=50"
+
+
 router.get('/', function(req, res) {
         
     const options = {
@@ -13,7 +25,7 @@ router.get('/', function(req, res) {
         }
       };
       
-      fetch('https://api.pandascore.co/lol/tournaments?sort=&page=1&per_page=50', options)
+      fetch("https://api.pandascore.co/leagues/4292/matches?filter[finished]=false&sort&page=1&per_page=50", options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
