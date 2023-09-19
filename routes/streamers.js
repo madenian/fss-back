11cc2d137e9933633287175dd2d336f9336636d7
Adrien AGARD <agardadrien@gmail.com>
2023-09-19 11:45:30 +0200

    ajout de la route post pour ajouter un nouveau streamer en BDD

--- routes/streamers.js
+++ routes/streamers.js
@@ -68,58 +68,120 @@ const fetch = require("node-fetch");
 //       }
 //     });
 
+router.get("/", async function (req, res) {
+  try {
+    const streamers = await Streamer.find().select(
+      "twitchId name broadcasterType description profileImage offlineImage createdAt"
+    );
 
-router.get('/', async function(req, res) {
-    try {
-        const streamers = await Streamer.find().select('twitchId name broadcasterType description profileImage offlineImage createdAt');
-        
-        const twitchClientId = 'yezyu92gzd8s4bpqxyrl1r893te09j'; // Remplacez par votre client ID Twitch
-        const twitchBearerToken = 'c4rczh7a6nniomgrnd42sybt14ivcw'; // Remplacez par votre jeton d'accès Twitch
-    
+    const twitchClientId = "yezyu92gzd8s4bpqxyrl1r893te09j"; // Remplacez par votre client ID Twitch
+    const twitchBearerToken = "c4rczh7a6nniomgrnd42sybt14ivcw"; // Remplacez par votre jeton d'accès Twitch
 
-        const fetchPromises = streamers.map(async (streamer) => {
-            try {
-                const response = await fetch(`https://api.twitch.tv/helix/schedule?broadcaster_id=${streamer.twitchId}`, {
-                    method: 'GET',
-                    headers: {
-                        'Client-ID': twitchClientId,
-                        'Authorization': `Bearer ${twitchBearerToken}`,
-                    },
-                });
+    const fetchPromises = streamers.map(async (streamer) => {
+      try {
+        const response = await fetch(
+          `https://api.twitch.tv/helix/schedule?broadcaster_id=${streamer.twitchId}`,
+          {
+            method: "GET",
+            headers: {
+              "Client-ID": twitchClientId,
+              Authorization: `Bearer ${twitchBearerToken}`,
+            },
+          }
+        );
 
-                if (response.status === 200) {
-                    const schedule = await response.json();
-                    // Vérifiez si le planning est présent et ajoutez-le à l'objet streamer.
-                    if (schedule.data && schedule.data.segments) {
-                        // Créez un nouvel objet streamer avec la propriété schedule mise à jour.
-                        return {
-                            ...streamer.toObject(),
-                            schedule: schedule.data.segments
-                        };
-                    }
-                } else {
-                    console.error(`Erreur lors de la récupération du planning pour ${streamer.name}: ${response.statusText}`);
-                }
-            } catch (error) {
-                console.error(`Erreur lors de la récupération du planning pour ${streamer.name}:`, error);
-            }
-
-            // Si aucune modification n'est apportée, retournez null.
-            return null;
-        });
-
-        const streamersWithSchedule = (await Promise.all(fetchPromises)).filter(streamer => streamer !== null);
-
-        if (streamersWithSchedule.length > 0) {
-            res.json(streamersWithSchedule);
+        if (response.status === 200) {
+          const schedule = await response.json();
+          // Vérifiez si le planning est présent et ajoutez-le à l'objet streamer.
+          if (schedule.data && schedule.data.segments) {
+            // Créez un nouvel objet streamer avec la propriété schedule mise à jour.
+            return {
+              ...streamer.toObject(),
+              schedule: schedule.data.segments,
+            };
+          }
         } else {
-            // Aucun streamer avec planning trouvé, renvoyez un message d'erreur approprié.
-            res.status(404).json({ error: 'Aucun streamer avec planning trouvé.' });
+          console.error(
+            `Erreur lors de la récupération du planning pour ${streamer.name}: ${response.statusText}`
+          );
         }
-    } catch (error) {
-        console.error('Erreur lors de la récupération des streamers et des plannings :', error);
-        res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+      } catch (error) {
+        console.error(
+          `Erreur lors de la récupération du planning pour ${streamer.name}:`,
+          error
+        );
+      }
+
+      // Si aucune modification n'est apportée, retournez null.
+      return null;
+    });
+
+    const streamersWithSchedule = (await Promise.all(fetchPromises)).filter(
+      (streamer) => streamer !== null
+    );
+
+    if (streamersWithSchedule.length > 0) {
+      res.json(streamersWithSchedule);
+    } else {
+      // Aucun streamer avec planning trouvé, renvoyez un message d'erreur approprié.
+      res.status(404).json({ error: "Aucun streamer avec planning trouvé." });
     }
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération des streamers et des plannings :",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des données." });
+  }
+});
+
+//route POST pour rechercher s'il n'est pas déjà présent en BDD et ajouter un streamer en BDD si besoin
+router.post("/", function (req, res) {
+  const streamer = req.body;
+
+  Streamer.findOne({ name: { $regex: new RegExp("^" + streamer.name, "i") } }).then((streamerFound) => {
+    if (streamerFound) {
+      res.json({
+        result: false,
+        streamer: streamerFound,
+        message: "Streamer déjà présent en BDD",
+      });
+    } else {
+      //formattage du nom en toLowerCase
+      const cleanName = streamer.name.toLowerCase();
+      const apiKey = "yezyu92gzd8s4bpqxyrl1r893te09j";
+      const accessToken = "c4rczh7a6nniomgrnd42sybt14ivcw";
+      const url = `https://api.twitch.tv/helix/users?login=${cleanName}`;
+      const headers = {
+        "Client-ID": apiKey,
+        Authorization: `Bearer ${accessToken}`,
+      };
+      fetch(url, { headers })
+        .then((res) => res.json())
+        .then((twitchData) => {
+          if (twitchData) {
+            const newStreamer = new Streamer({
+              twitchId: twitchData.data[0].id,
+              name: twitchData.data[0].display_name,
+              broadcasterType: twitchData.data[0].broadcaster_type,
+              description: twitchData.data[0].description,
+              profileImage: twitchData.data[0].profile_image_url,
+              offlineImage: twitchData.data[0].offline_image_url,
+              createdAt: twitchData.data[0].created_at,
+            });
+            newStreamer.save().then(() => {
+              res.json({
+                result: true,
+                streamer: newStreamer,
+                message: "Streamer ajouté en BDD",
+              });
+            });
+          }
+        });
+    }
+  });
 });
 
 module.exports = router;
