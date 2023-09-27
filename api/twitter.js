e1aa8d7c533537b9ad081a51d7aac011cdbd6e43
Thibaud <abonnel.t@gmail.com>
2023-09-27 12:40:10 +0200

    bot v2

--- api/twitter.js
+++ api/twitter.js
@@ -3,6 +3,7 @@ const { twitterClient } = require("../routes/twitterClient");
 const { generateTweet } = require("../routes/randomTweet");
 
 async function handler(req, res) {
+   
     await twitterClient.v2.tweet(generateTweet());
 
     res.status(200).json({ message: "successfully tweeted" });
